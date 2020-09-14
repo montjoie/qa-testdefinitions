67d59710fae4eb05fcd4656b6b204caa9506df7d
Jan-Simon Moeller <jsmoeller@linuxfoundation.org>
2020-09-14 15:53:21 +0200

    Change regular pyagl invocation to nohw
    In CI we will execute the 'not hwrequired' flag.
    Make this the default case in the test-suites.
    
    Bug-AGL: SPEC-3572
    Signed-off-by: Jan-Simon Moeller <jsmoeller@linuxfoundation.org>
    Change-Id: I370bb3c12953d7455a1a4c06c573cbc10ee17bda

---
+++ common/scripts/pyagl-nohw-lava.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# for now play safe and only expect ethernet
+export AGL_AVAILABLE_INTERFACES="ethernet"
+
+pytest --color=no --show-capture=no -k "not hwrequired" /usr/lib/python?.?/site-packages/pyagl/tests/ -L
+
+
