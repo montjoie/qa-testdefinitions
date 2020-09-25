a649e671459be330c7f49d3501a595c0cb4ab836
Corentin LABBE <clabbe@baylibre.com>
2020-09-25 13:41:41 +0200

    SPEC-3484: Use the new gcovr-wrapper
    For using gcov, a new gcovr-wrapper was created.
    Let's use it, capture its output ang generate some metrics.
    
    Bug-AGL: SPEC-3484
    Change-Id: Idbd9aa34d37d31414d18341b035440e28df5dc83
    Signed-off-by: Corentin LABBE <clabbe@baylibre.com>

--- common/scripts/application-lifecycle.sh
+++ common/scripts/application-lifecycle.sh
@@ -391,21 +391,18 @@ do
 		inspect_wgt $WGTNAME-debug.wgt $WGTNAME
 		do_release_test $WGTNAME $WGTNAME-debug.wgt
 	fi
-	if [ -e $WGTNAME-coverage.wgt ];then
-		inspect_wgt $WGTNAME-coverage.wgt $WGTNAME
-		do_release_test $WGTNAME $WGTNAME-coverage.wgt
-		check_service_running $WGTNAME
-		if [ $? -eq 1 ];then
-			afm-util install $TOPDIR/$WGTNAME-test.wgt
-			do_afm_test $TOPDIR/$WGTNAME-test.wgt
-			if [ $? -eq 0 ];then
-				lava-test-case run-test-$WGTNAME --result pass
-			else
-				lava-test-case run-test-$WGTNAME --result fail
-			fi
+	if [ -e "$WGTNAME-coverage.wgt" ];then
+		gcovr-wrapper "$WGTNAME-coverage.wgt" > coverage.result
+		RET=$?
+		cat coverage.result
+		if [ $RET -eq 0 ];then
+			lava-test-case "run-test-$WGTNAME-coverage" --result pass
+			LINES_PERCENT=$(grep -o '^lines.*%' coverage.result | cut -d ' ' -f2 | cut -d% -f1)
+			lava-test-case "run-test-$WGTNAME-coverage-lines" --result pass --measurement "$LINES_PERCENT"
+			BRANCHES_PERCENT=$(grep -o '^branches.*%' coverage.result | cut -d ' ' -f2 | cut -d% -f1)
+			lava-test-case "run-test-$WGTNAME-coverage-branches" --result pass --measurement "$BRANCHES_PERCENT"
 		else
-			echo "DEBUG: $WGTNAME is not running, skipping test"
-			lava-test-case run-test-$WGTNAME --result skip
+			lava-test-case "run-test-$WGTNAME-coverage" --result fail
 		fi
 	fi
 done
