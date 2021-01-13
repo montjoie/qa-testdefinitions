5dfcc6c0441fc9b7fc7b84eae0188301fd658775
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2021-01-13 17:34:39 +0100

    Update pyagl testdefinition to set AGL_CAN_INTERFACE
    We need to use vcan0 for the canreplay.
    
    Bug-AGL: SPEC-3756
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>
    Change-Id: I8e3e8c7d6eadf081014e129cfd59ad217004f46d

--- common/scripts/pyagl-nohw-lava.sh
+++ common/scripts/pyagl-nohw-lava.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # for now play safe and only expect ethernet
 export AGL_AVAILABLE_INTERFACES="ethernet"
+export AGL_CAN_INTERFACE="vcan0"
 
 pytest --color=no --show-capture=no -k "not hwrequired and not internet" /usr/lib/python?.?/site-packages/pyagl/tests/ -L
 
