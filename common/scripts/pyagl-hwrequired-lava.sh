0471b588cd08ac0674413e7c86598101ce11dd25
Corentin LABBE <clabbe@baylibre.com>
2021-02-17 14:46:03 +0000

    radio
    Change-Id: I260b29b371b76843d462475b0d50e0bab56132ca

--- common/scripts/pyagl-hwrequired-lava.sh
+++ common/scripts/pyagl-hwrequired-lava.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-pytest --show-capture=no --color=no -k hwrequired /usr/lib/python?.?/site-packages/pyagl/tests/ -L
+#pytest --show-capture=no --color=no -k radio /usr/lib/python?.?/site-packages/pyagl/tests/ -L
 
+pytest --color=no /usr/lib/python?.?/site-packages/pyagl/tests/test_radio.py
 
+echo "=== RADIO DONGLE USED ==="
+journalctl -b | grep agl-service-radio
+echo "=== RADIO DONGLE USED ==="
+lsusb
+zcat /proc/config.gz | grep USB
