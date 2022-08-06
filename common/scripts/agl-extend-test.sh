2e5067e302bcfe08f970212f8d5c183561bf1f55
duerpei <duep.fnst@fujitsu.com>
2022-08-06 11:47:36 +0800

    qa-testdefinitions: add yaml and script for agl extend test
    In qa-testdefinitions, add lava testdefinition (yaml+script)
    for agl extend test
    
    Bug-AGL: SPEC-4345
    
    Signed-off-by: duerpei <duep.fnst@fujitsu.com>
    Change-Id: I2df480b5e880c62868c17192c827e8f7ccc2a749

---
+++ common/scripts/agl-extend-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+pre_check=`which agl-test`
+
+if [ -n "$pre_check" ] ; then
+    agl-test
+    echo "agl-extend-test is present"
+else
+    echo "agl-test is not installed, abort this test"
+    exit 127
+fi
+
+
+if [ -x ./artiproxy-upload.sh ] ; then
+    LOG_DIR=/var/run/agl-test/logs/log-to-report
+    ZIP_FILE=`ls ${LOG_DIR} | grep agl-test-log*`
+
+    if [ -z $ZIP_FILE ] ; then
+        echo "Cannot find agl-extend-test log"
+        exit 1
+    fi
+
+    ./artiproxy-upload.sh $LOG_DIR $ZIP_FILE
+    if [ $? -eq 1 ] ; then
+        echo "Upload of ${ZIP_FILE} failed"
+        exit 1
+    else
+        echo "Upload of test report successful"
+        exit 0
+    fi
+
+else
+    echo "The file artiproxy-upload.sh does not exist"
+    exit 126
+fi
